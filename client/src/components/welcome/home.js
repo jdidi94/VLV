b4ea7ab0f2786f1c63a3dcf27cc0fcc11b887d12
jdidi94 <jdididaoud404@gmail.com>
2022-02-16 20:28:41 +0100

    calendar

--- client/src/components/welcome/home.js
+++ client/src/components/welcome/home.js
@@ -1,13 +1,17 @@
 import React from "react";
 // import agent from './agent'
-import MenuBar from "./menuBar"
-import Searching from './searching'
+import Booking from "./booking";
+import MenuBar from "./menuBar";
+import Searching from "./searching";
 // import { Route, Switch } from 'react-router-dom';
 const Home = () => {
-  return <div className="container">
-  <MenuBar/>
-  <Searching/>
-  </div>;
+  return (
+    <div className="container">
+      <MenuBar />
+      <Searching />
+      <Booking />
+    </div>
+  );
 };
 
 export default Home;
